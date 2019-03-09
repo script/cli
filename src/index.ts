f83e3ad6ded59070e59c60ecba31407b4c647989
Grant Timmerman <timmerman+devrel@google.com>
2019-03-08 19:07:44 -0800

    Add basic commander setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+
+import * as commander from 'commander';
+
+const PROJECT_NAME = 's';
+
+commander
+  .name(PROJECT_NAME)
+  .usage(`<command> [options]`)
+  .description(`${PROJECT_NAME} - The Script CLI`);
+
+// defaults to help if commands are not provided
+if (!process.argv.slice(2).length) {
+  commander.outputHelp();
+}
+// User input is provided from the process' arguments
+commander.parse(process.argv);
